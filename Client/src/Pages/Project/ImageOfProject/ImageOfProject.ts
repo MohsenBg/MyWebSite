cf0da4c17a6d17aa3080d2f474a67e25ac2476a4
Mohsen <moh.1380.1393@gmail.com>
2021-06-20 05:09:35 -0700

    project Page
    imageSidebugFix

--- Client/src/Pages/Project/ImageOfProject/ImageOfProject.ts
+++ Client/src/Pages/Project/ImageOfProject/ImageOfProject.ts
@@ -1,38 +1,47 @@
+import { IconType } from "react-icons/lib";
 import image01 from "../../../Images/Project/ImageSideProject/Image01.png";
 import image02 from "../../../Images/Project/ImageSideProject/Image02.png";
 import image03 from "../../../Images/Project/ImageSideProject/Image03.png";
 import image04 from "../../../Images/Project/ImageSideProject/Image04.png";
 import image05 from "../../../Images/Project/ImageSideProject/Image05.png";
 import image06 from "../../../Images/Project/ImageSideProject/Image06.png";
+import * as GoIcons from "react-icons/go";
 
 interface img {
   id: number;
   imageUrl: string;
+  dote: IconType;
 }
 
 export const Image: Array<img> = [
   {
     id: 1,
     imageUrl: image01,
+    dote: GoIcons.GoPrimitiveDot,
   },
   {
     id: 2,
     imageUrl: image02,
+    dote: GoIcons.GoPrimitiveDot,
   },
   {
     id: 3,
     imageUrl: image03,
+    dote: GoIcons.GoPrimitiveDot,
   },
   {
     id: 4,
     imageUrl: image04,
+    dote: GoIcons.GoPrimitiveDot,
   },
   {
     id: 5,
     imageUrl: image05,
+    dote: GoIcons.GoPrimitiveDot,
   },
   {
     id: 6,
     imageUrl: image06,
+    dote: GoIcons.GoPrimitiveDot,
   },
 ];
--- Client/src/Pages/Project/Project.tsx
+++ Client/src/Pages/Project/Project.tsx
@@ -1,22 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Project.scss";
 import * as AiIcons from "react-icons/ai";
-import * as GoIcons from "react-icons/go";
 import { Image } from "./ImageOfProject/ImageOfProject";
-import { url } from "inspector";
 
 const Project: React.FC = () => {
   const [currentImage, setCurrentImage] = useState<number>(1);
+  const [autoImageStatus, setAutoImageStatus] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (autoImageStatus) {
+      setTimeout(() => {
+        if (Image.length > currentImage) {
+          setCurrentImage(currentImage + 1);
+        } else {
+          setCurrentImage(1);
+        }
+      }, 7000);
+    }
+  }, [currentImage, autoImageStatus]);
 
   const nextImage = () => {
+    setAutoImageStatus(false);
     if (Image.length > currentImage) {
       setCurrentImage(currentImage + 1);
+    } else {
+      setCurrentImage(1);
     }
   };
 
   const previousImage = () => {
+    setAutoImageStatus(false);
     if (currentImage > 1) {
       setCurrentImage(currentImage - 1);
+    } else {
+      setCurrentImage(Image.length);
+    }
+  };
+
+  const autoShowImage = () => {
+    if (currentImage > 1) {
+      setAutoImageStatus(!autoImageStatus);
     }
   };
 
@@ -32,6 +55,16 @@ const Project: React.FC = () => {
             <div className="LeftArrow" onClick={() => previousImage()}>
               <AiIcons.AiFillLeftCircle />
             </div>
+            <div
+              className={
+                autoImageStatus
+                  ? "btnAutoImage btnAutoImageOn"
+                  : "btnAutoImage btnAutoImageOff"
+              }
+              onClick={() => autoShowImage()}
+            >
+              AutoShow {autoImageStatus ? "On" : "Off"}
+            </div>
             {Image.map((img) => (
               <div key={img.id}>
                 <img
@@ -41,12 +74,22 @@ const Project: React.FC = () => {
                       : "imageProjectSide sideImage"
                   }
                   src={img.imageUrl}
+                  alt="server didn't response"
                 />
               </div>
             ))}
-
-            <div className="dotIcons">
-              <GoIcons.GoPrimitiveDot />
+            <div className="doteContainer">
+              {Image.map((img) => (
+                <div key={img.id}>
+                  <div
+                    className={
+                      currentImage === img.id ? "dote doteActive" : "dote"
+                    }
+                  >
+                    <img.dote />
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
