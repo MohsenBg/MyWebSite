4f8bde8c18c8a47375d1c469615c9c67b6fdf220
Mohsen <moh.1380.1393@gmail.com>
2021-06-26 11:01:36 -0700

    Demo About Page Added

--- Client/src/Components/NavBar/NavItems/NavItem.ts
+++ Client/src/Components/NavBar/NavItems/NavItem.ts
@@ -1,32 +1,30 @@
-
 interface NavItem {
-       id:number,
-       itemName:string,
-       link:string
+  id: number;
+  itemName: string;
+  link: string;
 }
 
+export const NavItems: Array<NavItem> = [
+  {
+    id: 1,
+    itemName: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    itemName: "Project",
+    link: "/Project",
+  },
+  //{
+  //  id:3,
+  //itemName:"MySkill",
+  //link:"/MySkill"
 
+  //},
 
-export const NavItems:Array<NavItem>=[{
-    id:1,
-    itemName:"Home",
-    link:"/",
-},
-{
-    id:2,
-    itemName:"Project",
-    link:"/Project"
-},
-{
-    id:3,
-    itemName:"MySkill",
-    link:"/MySkill"
-   
-},
-{
-    id:4,
-    itemName:"About",
-    link:"/About"
-
-}
-] 
+  {
+    id: 4,
+    itemName: "About",
+    link: "/About",
+  },
+];
